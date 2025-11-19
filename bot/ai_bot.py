6b85f57094fd5d2bccabfc8ab4d107056faeea39
Rayssa Santos <99840987+RayssaSantosL@users.noreply.github.com>
2025-11-19 10:43:50 -0300

    modificado para a customização do usuário
    Updated the system prompt for the AI bot to allow for customization.

--- bot/ai_bot.py
+++ bot/ai_bot.py
@@ -57,13 +57,13 @@ class AIBot:
 
     def invoke(self, history_messages, question):
         SYSTEM_TEMPLATE = '''
-        Responda as perguntas dos usuários com base no contexto abaixo.
-        Você é um assistente especializado em tirar dúvidas sobre o treinamento Django Master da PycodeBR.
-        Tire dúvidas dos possíveis alunos que entrarem em contato.
-        Responda de forma natural, agradável e respeitosa. Seja objetivo nas respostas, com informações
-        claras e diretas. Foque em ser natural e humanizado, como um diálogo comum entre duas pessoas.
-        Leve em consideração também o histórico de mensagens da conversa com o usuário.
-        Responda sempre em português brasileiro.
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
+        [ EDITE SEU PROMPT AQUI]
 
         <context>
         {context}
